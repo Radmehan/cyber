02d325d19f140b789cdcdf158eab67d28065ac60
Radmehan <74257580+Radmehan@users.noreply.github.com>
2024-06-06 23:51:31 +0600

    add section

--- staticfiles/security/js/main.js
+++ staticfiles/security/js/main.js
@@ -12,6 +12,50 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 
+    // Testimonial-carousel
+    $(".testimonial-carousel").owlCarousel({
+        loop: true,
+        items: 1,
+        smartSpeed: 1500,
+        margin: 30,
+        dots: false,
+        nav: true,
+        navText: ['<i class="flaticon-left-chevron"></i>','<i class="flaticon-right-chevron"></i>'],
+    });
+
+    // Testimonial-carousel-2
+    var testOwl2 = $(".testimonial-carousel-2");
+    testOwl2.owlCarousel({
+        loop: true,
+        items: 3,
+        smartSpeed: 1500,
+        margin: 30,
+        dots: false,
+        nav: false,
+        responsive:{
+            0: {
+                items: 1
+            },
+            768: {
+                items: 2
+            },
+            992: {
+                items: 3
+            }
+        }
+    });
+    $(".testimonial-carousel-2-controller .carousel-prev-arrow").on("click", function () {
+        testOwl2.trigger('prev.owl.carousel', [300]);
+    })
+    $(".testimonial-carousel-2-controller .carousel-next-arrow").on("click", function () {
+        testOwl2.trigger('next.owl.carousel');
+    })
+    var windowTab = $(window).width();
+    if(windowTab < 992) {
+        $(".testimonial-carousel-2-controller").appendTo(".testimonial-carousel-2");
+    }
+
+
     /**
    * Initiate glightbox
    */
